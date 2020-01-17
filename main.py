158c57af7a2e3b19b35737f4c26b4d0c64f28cde
mishakrul <mkrul@spoonflower.com>
2020-01-16 21:58:47 -0500

    needs refactoring, but it kinda works

--- main.py
+++ main.py
@@ -2,38 +2,46 @@ import requests
 from bs4 import BeautifulSoup
 from googlesearch import search
 
-# enter some text
-# search for that text in google, with quotes
-# go to that link
-# text gets downcased
-# loop over each div on page, downcase the contents, compare to text that was passed in
-# # once there is a match return the div contents of the parent div
+# this scraper will only return data from quizlet.com!
 
 def main():
-  try:
-    query = input('Paste your text here and press [enter]:').lower()
+  query = input('Paste your text here and press [enter]: ').lower()
+
+  while query != '':
     google_search = search(query, tld='co.in', num=1, stop=1, pause=1)
     url = ''
+    answer = ''
 
     for result in google_search: 
       split_url = result.split('/')[:4]
       url = '/'.join(split_url) + '/' + 'print'
 
-      print(url)
-
     req = requests.get(url)
 
     soup = BeautifulSoup(req.content, features='html.parser')
 
-    spans = soup.findAll('span', {'class': 'left copy'})
+    left_spans = soup.findAll('span', {'class': 'left copy'})
 
-    for each in spans:
-      text = '"' + each.text.lower().strip() + '"'
-      if query in text:
+    for each in left_spans:
+      raw_text = each.text.lower().strip()
+      quoted_text = '"' + raw_text + '"'
+      if query in raw_text or query in quoted_text:
         print(each.parent.text)
-
-  except:
-    idklol()
+        answer = each.parent.text
+    
+    if answer == '':
+      right_spans = soup.findAll('span', {'class': 'right copy'})
+      for each in right_spans:
+        raw_text = each.text.lower().strip()
+        quoted_text = '"' + raw_text + '"'
+        if query in raw_text or query in quoted_text:
+          print(each.parent.text)
+          answer = each.parent.text
+    
+    if answer == '':
+      idklol()
+  
+    query = input('Paste your text here and press [enter]: ').lower()
 
 def idklol():
   print("¯\\_(ツ)_/¯")
